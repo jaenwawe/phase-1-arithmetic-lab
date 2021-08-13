54207f2e3a0333a38d9d840aeebb4ea2b02e608e
jaenwawe <nwawe.jae@gmail.com>
2021-08-13 15:22:51 -0500

    commit tree 15897cbdbb1d0ed23a43cb9664e97239406183ea

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=2, num2 =31, random;
+let multiply = num1 * num2;
 
-// Write your code here
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max =  Math.max (num1, 15, 20);
+random = Math.floor(Math.random() * 11);
+
+console.log(multiply);
+console.log(mod);
+console.log(max);
+console.log(random);
+
